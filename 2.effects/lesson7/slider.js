a643aec11414384fab1b3f3b85fe12d30637831e
Peter Hewitson <pjhewitson@hotmail.com>
2013-02-08 09:39:25 -0500

    lesson 7 complete

--- 2.effects/lesson7/slider.js
+++ 2.effects/lesson7/slider.js
@@ -8,10 +8,44 @@
         current = 1,
         totalImgsWidth = imgsLen * imgWidth;  //2400
     
+    console.log('# of images: ' + imgsLen);
+    
     $('#slider-nav').show().find('button').on('click', function() {
-        console.log('clicked');
+        var direction = $(this).data('dir'),
+            loc = imgWidth;     // 600
+        
+        // Update current value
+        ( direction === 'next' ) ? ++current : --current;
+        
+        //console.log(current);
+        
+        // If first image...
+        if (current === 0) {
+            current = imgsLen;
+            loc = totalImgsWidth - imgWidth;
+            direction = 'next';
+        } else if ( current - 1 === imgsLen ) {   // Are we at end? Should we reset?
+            current = 1;
+            loc = 0;
+        }
+        
+        // Call the animation function to slide the images.
+        transition( sliderUL, loc, direction );
     });
     
+    function transition ( container, loc, direction ) {
+        var unit; // -= or +=
+        
+        //console.log(loc);
+        
+        if ( direction && loc !== 0 ) {
+            unit = ( direction === 'next' ) ? '-=' : '+=';
+        }
+        
+        container.animate({
+            'margin-left': unit ? (unit + loc) : loc
+        });
+    }
     
 })(jQuery);
 // NOTE: Remember to include "jQuery" here to complete importing.
